d81d97eaaef4f1f03c1bd3bebfb4108d2c50ae1b
riley soloner <rileysoloner@gmail.com>
2022-07-03 17:59:56 -0400

    first deliverable

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,1 +1,22 @@
 //BookStore has been moved to data.js 
+
+// Declare function
+function renderHeader () {
+    document.querySelector('h1').textContent = bookStore.name
+}
+
+function renderFooter () {
+    const footerDivs =  document.querySelectorAll('footer div')
+   
+    // Bookstore Name
+    footerDivs[0].textContent = bookStore.name  
+    // Bookstore Address
+    footerDivs[1].textContent = bookStore.address
+    // Bookstore Hours
+    footerDivs[2].textContent = bookStore.hours 
+}
+
+// Invoke
+renderHeader()
+
+renderFooter()
