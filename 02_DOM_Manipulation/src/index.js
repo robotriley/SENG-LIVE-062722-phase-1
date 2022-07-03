0d9fb742cfa0bd2843a150cc37200e60bef99630
riley soloner <rileysoloner@gmail.com>
2022-07-03 18:40:46 -0400

    final touches for lecture 2

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,11 +1,9 @@
 //BookStore has been moved to data.js 
 
 // Declare function
-function renderHeader () {
-    document.querySelector('h1').textContent = bookStore.name
-}
+const renderHeader = () => document.querySelector('h1').textContent = bookStore.name
 
-function renderFooter () {
+const renderFooter = () => {
     const footerDivs =  document.querySelectorAll('footer div')
    
     // Bookstore Name
@@ -16,7 +14,35 @@ function renderFooter () {
     footerDivs[2].textContent = bookStore.hours 
 }
 
+// const createBook = () => {
+
+// }
+
+const renderBook = book => {
+    // Generate HTML elements
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const pAuthor = document.createElement('p')
+    const pPrice = document.createElement('p')
+    const img = document.createElement('img')
+    const btn = document.createElement('button')
+    // Fill those HTML elements with content
+    h3.textContent = book.title
+    pAuthor.textContent = book.author
+    pPrice.textContent = book.price
+    btn.textContent = 'Delete Book'
+    img.src = book.imageUrl
+    li.className = 'list-li'
+// Generate full <li> for each book
+    li.append(h3, pAuthor, pPrice, img, btn)
+    document.querySelector('#book-list').append(li)
+
+
+};
 // Invoke
 renderHeader()
 
 renderFooter()
+
+const books = bookStore.inventory
+books.forEach(renderBook);
