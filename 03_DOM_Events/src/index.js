f2c4ddf303826148ca2b36a293196e426336bc48
riley soloner <rileysoloner@gmail.com>
2022-07-04 16:49:22 -0400

    cmon

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,8 +1,10 @@
+// document.querySelector('h1').addEventListener('click', e => console.log(e.target))
+document.addEventListener('DOMcontentLoaded', () => {
 
 renderHeader()
 renderFooter()
 bookStore.inventory.forEach(renderBookCard)
-
+document.querySelector('#book-form').addEventListener('submit', handleForm)
 
 // Renders Header
 function renderHeader(){
@@ -28,6 +30,8 @@ function renderBookCard(cardData) {
     pAuthor.textContent = cardData.author
     pPrice.textContent = `$${cardData.price}`
     btn.textContent = 'Delete'
+    // btn.addEventListener('click', (e) => e.target.parentElement.remove())
+    btn.addEventListener('click', () => li.remove())
 
     img.src = cardData.imageUrl
     li.className = 'list-li'
@@ -36,3 +40,23 @@ function renderBookCard(cardData) {
     document.querySelector('#book-list').append(li)
 }
 
+function handleForm(e) {
+    e.preventDefault()
+    
+    const book = {
+    
+        id: bookStore.inventory.length + 1,
+        title: e.target.title.value,
+        author: e.target.author.value,
+        price: e.target.price.value,
+        reviews: [],
+        inventory: e.target.inventory.value,
+        imageUrl: e.target.imageUrl.value
+        }
+
+    bookStore.inventory.push(book);
+
+    console.log(bookStore.inventory);
+    renderBookCard(book);
+}
+});
